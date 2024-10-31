dfde8253f297b841540edec56ed028d3ac2160b2
Avadhut Noola <avdhutnula@gmail.com>
2024-10-31 12:47:32 +0530

    Using Loop

---
+++ LanguageProblems/FibonacciSequence.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class FibonacciSequence {
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter nth term for sequence: ");
+		int n = sc.nextInt();
+		
+		//Using Loop
+		int first = 0; int second = 1;
+		int third;
+		System.out.println("Fibonacci sequence for nth term: ");
+		
+		for(int i=2; i<=n; i++) {
+			third = first + second;
+			
+			System.out.print(third + " ");
+			
+			first= second; second= third;
+		}
+		
+	}
+}
