237b05f23bd2c1c9b810b34e1e2faf61dc36c376
Avadhut Noola <avdhutnula@gmail.com>
2024-11-14 20:08:46 +0530

    Converting Binary to Decimal

---
+++ functions/binToDec.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class binToDec {
+
+    public static int binToDecimal(int binN) {
+        int power = 0;
+        int decNum = 0;
+
+        while ( binN > 0 ) {
+            int lastDig = binN % 10;
+            decNum = decNum + (lastDig * (int) Math.pow(2, power));
+
+            power++;
+            binN /= 10; // Reduce 1 digit after each iteration
+        }
+
+        return decNum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a binary no: ");
+        int binN = sc.nextInt();
+        System.out.println("Decimal no for binary " + binN + " = " + binToDecimal(binN));
+    }
+}
